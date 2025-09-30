99dca26fec9918c9635ea823bb6e99b9d855b0fe
Radchenko Marina <mar.radchenko@gmail.com>
2025-09-30 14:44:32 +0300

    add header component and simple auth store

---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Header />
+
+        <main class="container">
+            <router-view />
+        </main>
+    </div>
+</template>
+
+<script setup>
+import Header from './common/HeaderComponent.vue'
+</script>
