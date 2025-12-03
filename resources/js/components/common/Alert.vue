7aca8945a71ee0726652ab622bceaab9921685ed
Radchenko Marina <mar.radchenko@gmail.com>
2025-12-03 17:47:37 +0200

    clear alert message

--- resources/js/components/common/Alert.vue
+++ resources/js/components/common/Alert.vue
@@ -1,12 +1,12 @@
 <template>
     <div v-if="alertText" class="alert mt-2 alert-dismissible" :class="alertClass" role="alert">
         {{ alertText }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" @click="clearAlert()" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 </template>
 
 <script setup>
-import { alertText, alertType } from '../../store/alertMessages.js'
+import { alertText, alertType, clearAlert } from '../../store/alertMessages.js'
 import { computed } from 'vue'
 
 const alertClass = computed(() =>
